b8ec2c4e3c138af62f8948982567fbfc14db2f56
MK <mirjana_1991@hotmail.com>
2019-09-25 10:00:24 +0200

    undefined

--- src/reducers/error.reducer.js
+++ src/reducers/error.reducer.js
@@ -1,12 +1,13 @@
-import {ADD_ERROR, REMOVE_ERROR} from '../actions/action.types';
-let errorReducer = function (state =false, action){ 
-    switch(action.type){
-      case ADD_ERROR:
-        return  action.item ;
-      case REMOVE_ERROR:
-        return false;
-      default:
-        return state;      
-    }
+import { ADD_ERROR, REMOVE_ERROR } from '../actions/action.types';
+let errorReducer = function (state = false, action) {
+  switch (action.type) {
+    case ADD_ERROR:
+      if (action.item === undefined) return state;
+      else return action.item;
+    case REMOVE_ERROR:
+      return false;
+    default:
+      return state;
+  }
 }
-export {errorReducer}
+export { errorReducer }
